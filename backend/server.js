8fdd96a2956d9459c739c68b2abfb27c71abf674
Erdem <erdemootv1@gmail.com>
2025-05-21 14:15:07 +0300

    auth işlemleri tamamlandı: register ve login eklendi

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 // server.js
 const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
+const authRoutes = require("../backend/routes/authRoutes");
+
 
 // .env dosyasını yükle
 dotenv.config();
@@ -12,9 +14,16 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// JSON gövdelerini parse etmek için middleware
+app.use(express.json());
+
+//Test endpoint
 app.get('/', (req, res) => {
   res.send('API çalışıyor...');
 });
+
+//Auth Route
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server ${PORT} portunda çalışıyor`);
